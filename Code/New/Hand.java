65aaefcd2f1a2e0ea47247a6a974317d1355c5f1
Avin Tiletile <atiletile@horizon.csueastbay.edu>
2020-04-10 17:25:56 -0700

    Added skeleton & comments.

--- Code/New/Hand.java
+++ Code/New/Hand.java
@@ -1,2 +1,29 @@
-public class Hand {
+public class Hand
+{
+	// hand full of cards
+	private Card[] hand;
+
+	// not sure what this is suppose to do
+	public void handValue()
+	{
+
+	}
+
+	// draw card
+	public void drawCard()
+	{
+
+	}
+
+	// checks if hand is blackjack, perhaps change to boolean?
+	public void isBlackJack()
+	{
+
+	}
+
+	// check if hand is bust, perhaps change to boolean?
+	public void isBust()
+	{
+
+	}
 }
