ae39f59f3e4c714b8de9e85b66e23d4a93a348f8
Avin Tiletile <atiletile@horizon.csueastbay.edu>
2020-05-01 19:03:20 -0700

    Ask for number of players added.

--- Code/New/BlackJackDriver.java
+++ Code/New/BlackJackDriver.java
@@ -1,27 +1,33 @@
 import java.util.Vector;
+import java.util.Scanner;
 
-public class BlackJackDriver {
-    public static void main(String[] args){
+public class BlackJackDriver
+{
+	public static void main(String[] args)
+	{
+		/*BlackJackGUI frame = new BlackJackGUI();
+		frame.display();*/
 
-    /*BlackJackGUI frame = new BlackJackGUI();
-    frame.display();*/
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("How many players?");
+		int playerNum = scan.nextInt();
+		
+		
+		Vector<Player> players = new Vector<>();
+		players.add(new Player("Alex", "abc", new Balance(2500, true)));
+		Dealer dealer = new Dealer();
+		Game game = new Game(dealer, players, playerNum);
+		game.newTable();
 
-    Vector<Player> players = new Vector<>();
-    players.add(new Player("Alex", "abc", new Balance(2500, true)));
-    Dealer dealer = new Dealer();
-    Game game = new Game(dealer, players, 1);
-    game.newTable();
-
-    /*Vector<Card>cards = new Vector<>();
-    cards.add(new Card(Suit.SPADE, Rank.ACE, Rank.ACE.getValue()));
-    cards.add(new Card(Suit.SPADE, Rank.EIGHT, Rank.EIGHT.getValue()));
-    cards.add(new Card(Suit.SPADE, Rank.TWO, Rank.TWO.getValue()));
-    cards.add(new Card(Suit.HEART, Rank.ACE, Rank.ACE.getValue()));
-    cards.add(new Card(Suit.CLUB, Rank.ACE, Rank.ACE.getValue()));
-    cards.add(new Card(Suit.DIAMOND, Rank.ACE, Rank.ACE.getValue()));
-
-    Hand hand = new Hand(cards);
-    System.out.println(hand.handValue());*/
-
-  }
+		Vector<Card>cards = new Vector<>();
+		cards.add(new Card(Suit.SPADE, Rank.ACE, Rank.ACE.getValue()));
+		cards.add(new Card(Suit.SPADE, Rank.EIGHT, Rank.EIGHT.getValue()));
+		cards.add(new Card(Suit.SPADE, Rank.TWO, Rank.TWO.getValue()));
+		cards.add(new Card(Suit.HEART, Rank.ACE, Rank.ACE.getValue()));
+		cards.add(new Card(Suit.CLUB, Rank.ACE, Rank.ACE.getValue()));
+		cards.add(new Card(Suit.DIAMOND, Rank.ACE, Rank.ACE.getValue()));
+		Hand hand = new Hand(cards);
+		System.out.println(hand.handValue());
+	}
 }
