af8f7047edb167f60cbba8dae9f619b62f00cab0
Avin Tiletile <atiletile@horizon.csueastbay.edu>
2020-05-04 14:59:10 -0700

    Skeleton

---
+++ Code/TestCode/RankTest.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RankTest
+{
+	@Test
+	public void valueIsEqual()
+	{
+		
+	}
+	
+	@Test
+	public void valueIsNotNull()
+	{
+		
+	}
+}
