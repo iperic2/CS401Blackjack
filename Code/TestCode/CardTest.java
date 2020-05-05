6b2558e68b6d492c453d437a0e5ddd33cffe4623
Avin Tiletile <atiletile@horizon.csueastbay.edu>
2020-05-05 09:55:29 -0700

    Organized. Anything left to add?

--- Code/TestCode/CardTest.java
+++ Code/TestCode/CardTest.java
@@ -1,22 +1,43 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class CardTest {
-
+public class CardTest
+{
     @Test
-    public void testConstructor() {
+    public void testConstructor()
+    {
         Card empty = new Card();
         Card kingSpade = new Card(Suit.SPADE, Rank.KING, 13);
         assertNotNull(empty);
         assertNotNull(kingSpade);
-    }
+    } 
 
     @Test
-    public void testGetters() {
-        Card aceSpade = new Card(Suit.SPADE, Rank.ACE, 1);
+    public void setIsNotNull()
+    {
+    	Card tester = new Card(Suit.SPADE, Rank.ACE, 1);
+    	
+    	assertNotNull(tester);
+    }
+    
+    @Test
+    public void suitIsEqualToGet()
+    {
+    	Card aceSpade = new Card(Suit.SPADE, Rank.ACE, 1);
         assertEquals(Suit.SPADE, aceSpade.getSuit());
+    }
+    
+    @Test
+    public void rankIsEqualToGet()
+    {
+    	Card aceSpade = new Card(Suit.SPADE, Rank.ACE, 1);
         assertEquals(Rank.ACE, aceSpade.getRank());
+    }
+    
+    @Test
+    public void valueIsEqualToGet()
+    {
+    	Card aceSpade = new Card(Suit.SPADE, Rank.ACE, 1);
         assertEquals(1, aceSpade.getValue());
     }
-
 }
