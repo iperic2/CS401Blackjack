de1a347b661aaaea91b6b737579264ca73da7349
Avin Tiletile <atiletile@horizon.csueastbay.edu>
2020-05-11 11:52:31 -0700

    Added comments.

--- Code/TestCode/RankTest.java
+++ Code/TestCode/RankTest.java
@@ -1,15 +1,18 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
 class RankTest
 {
+	// Test value returns expected.
 	@Test
 	public void valueIsEqual()
 	{
 		
 	}
 	
+	// Test value is not null.
 	@Test
 	public void valueIsNotNull()
 	{
