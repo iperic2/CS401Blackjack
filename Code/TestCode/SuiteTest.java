c16458f5af7e138078fe7c7c49735fa2456fa6b4
Avin Tiletile <atiletile@horizon.csueastbay.edu>
2020-05-11 12:41:17 -0700

    Suite.

---
+++ Code/TestCode/SuiteTest.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({CardTest.class, DealerTest.class, DeckTest.class, GameServerTest.class, GameStateTest.class,
+					HandTest.class, MoneyTest.class, PlayerTest.class, RankTest.class, RulesTest.class, UserTest.class})
+public class SuiteTest
+{
+	
+}
